a5a8331858d5b3000371f971ead854f3588efa4b
Eliaswalle <wael22nq@student.ju.se>
2024-08-30 15:57:56 +0200

    update

--- experiments/example-elias-2.js
+++ experiments/example-elias-2.js
@@ -1,23 +1,23 @@
 function setup() {
   createCanvas(600, 600);
+  //help from Jim on what i needed to have random colors
+  färg = [
+    color(255, 0, 0), // Red
+    color(255, 165, 0), // Orange
+    color(255, 255, 0), // Yellow
+    color(0, 255, 0), // Green
+    color(0, 0, 255), // Blue
+    color(75, 0, 130), // Indigo
+    color(238, 130, 238), // Violet (Purple)
+  ];
 }
-
+let färg;
 const size = 100;
 const layers = 30;
 
 function getRandomValue(pos, variance) {
   return pos + map(Math.random(), 0, 1, -variance, variance);
 }
-//help from Jim on what i needed to have random colors
-const färg = [
-  color(255, 0, 0), // Red
-  color(255, 165, 0), // Orange
-  color(255, 255, 0), // Yellow
-  color(0, 255, 0), // Green
-  color(0, 0, 255), // Blue
-  color(75, 0, 130), // Indigo
-  color(238, 130, 238), // Violet (Purple)
-];
 
 function drawLayers(x, y, size, layers) {
   const variance = size / 2.6;
