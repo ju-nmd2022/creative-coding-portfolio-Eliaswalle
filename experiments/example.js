f2c26d0f240c009b462619ed193dbd3589620f24
Eliaswalle <wael22nq@student.ju.se>
2024-09-02 08:18:15 +0200

    remove example (garrit)

--- experiments/example.js
+++ experiments/example.js
@@ -1,35 +1,35 @@
-function setup() {
-  createCanvas(innerWidth, innerHeight);
-  background(34, 39, 46);
-}
+// function setup() {
+//   createCanvas(innerWidth, innerHeight);
+//   background(34, 39, 46);
+// }
 
-function draw() {
-  background(34, 39, 46, 40);
-  noStroke();
-  fill(138, 12, 255);
+// function draw() {
+//   background(34, 39, 46, 40);
+//   noStroke();
+//   fill(138, 12, 255);
 
-  push();
-  translate(width / 2, height / 2);
+//   push();
+//   translate(width / 2, height / 2);
 
-  push();
-  rotate(frameCount / 13);
-  rect(25, 0, 50);
-  pop();
+//   push();
+//   rotate(frameCount / 13);
+//   rect(25, 0, 50);
+//   pop();
 
-  push();
-  rotate(-frameCount / 12);
-  rect(75, 0, 50);
-  pop();
+//   push();
+//   rotate(-frameCount / 12);
+//   rect(75, 0, 50);
+//   pop();
 
-  push();
-  rotate(frameCount / 12);
-  rect(125, 0, 50);
-  pop();
+//   push();
+//   rotate(frameCount / 12);
+//   rect(125, 0, 50);
+//   pop();
 
-  push();
-  rotate(-frameCount / 14);
-  rect(175, 0, 50);
-  pop();
+//   push();
+//   rotate(-frameCount / 14);
+//   rect(175, 0, 50);
+//   pop();
 
-  pop();
-}
+//   pop();
+// }
